8762353765041338e4f3bf50efb6a2dd6589c2e8
Jason Finch <jason.finch@gmail.com>
2026-03-31 22:19:57 +1000

    .

--- src/AngleSharp.Benchmarks/Html5SpecBenchmark.cs
+++ src/AngleSharp.Benchmarks/Html5SpecBenchmark.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using AngleSharp.Dom;
 using AngleSharp.Html.Parser;
 using BenchmarkDotNet.Attributes;
@@ -7,15 +8,14 @@ using BenchmarkDotNet.Configs;
 
 namespace AngleSharp.Benchmarks
 {
-    [MemoryDiagnoser, GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByParams), ShortRunJob]
+    [MemoryDiagnoser, ShortRunJob]
     public class HtmlParseBenchmark
     {
         private static readonly HtmlParser _parser = new();
+        private static readonly UrlTests _websites = CreateWebsites();
+        private string _combined;
 
-        [ParamsSource(nameof(GetSources))]
-        public UrlTest UrlTest { get; set; }
-
-        public IEnumerable<UrlTest> GetSources()
+        private static UrlTests CreateWebsites()
         {
             var websites = new UrlTests(".html", true);
             websites.Include(
@@ -30,47 +30,51 @@ namespace AngleSharp.Benchmarks
                 "https://en.wikipedia.org/wiki/Cascading_Style_Sheets",
                 "https://www.reuters.com/"
             ).GetAwaiter().GetResult();
-            return websites.Tests;
-        }
-
-        [Benchmark]
-        public IDocument Parse()
-        {
-            return _parser.ParseDocument(UrlTest.Source);
-        }
-    }
-
-    [MemoryDiagnoser, GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByParams), ShortRunJob]
-    public class HtmlSelectorBenchmark
-    {
-        private static readonly HtmlParser _parser = new();
-        private IDocument _document;
-
-        [ParamsSource(nameof(GetSources))]
-        public UrlTest UrlTest { get; set; }
-
-        public IEnumerable<UrlTest> GetSources()
-        {
-            var websites = new UrlTests(".html", true);
-            websites.Include(
-                "https://www.amazon.com",
-                "https://www.reddit.com",
-                "https://www.w3.org/TR/html5/single-page.html",
-                "https://en.wikipedia.org/wiki/South_African_labour_law",
-                "https://www.time.com",
-                "https://github.com/trending",
-                "https://html.spec.whatwg.org/",
-                "https://developer.mozilla.org/en-US/docs/Web/HTML",
-                "https://en.wikipedia.org/wiki/Cascading_Style_Sheets",
-                "https://www.reuters.com/"
-            ).GetAwaiter().GetResult();
-            return websites.Tests;
+            return websites;
         }
 
         [GlobalSetup]
         public void GlobalSetup()
         {
-            _document = _parser.ParseDocument(UrlTest.Source);
+            _combined = String.Concat(_websites.Tests.Select(t => t.Source));
+        }
+
+        [Benchmark]
+        public IDocument Parse()
+        {
+            return _parser.ParseDocument(_combined);
+        }
+    }
+
+    [MemoryDiagnoser, ShortRunJob]
+    public class HtmlSelectorBenchmark
+    {
+        private static readonly HtmlParser _parser = new();
+        private static readonly UrlTests _websites = CreateWebsites();
+        private IDocument _document;
+
+        private static UrlTests CreateWebsites()
+        {
+            var websites = new UrlTests(".html", true);
+            websites.Include(
+                "https://www.amazon.com",
+                "https://www.reddit.com",
+                "https://www.w3.org/TR/html5/single-page.html",
+                "https://www.time.com",
+                "https://github.com/trending",
+                "https://html.spec.whatwg.org/",
+                "https://developer.mozilla.org/en-US/docs/Web/HTML",
+                "https://en.wikipedia.org/wiki/Cascading_Style_Sheets",
+                "https://www.reuters.com/"
+            ).GetAwaiter().GetResult();
+            return websites;
+        }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var combined = String.Concat(_websites.Tests.Select(t => t.Source));
+            _document = _parser.ParseDocument(combined);
         }
 
         [Benchmark]
@@ -87,5 +91,17 @@ namespace AngleSharp.Benchmarks
 
         [Benchmark]
         public IHtmlCollection<IElement> Headings() => _document.QuerySelectorAll("h1, h2, h3, h4, h5, h6");
+
+        [Benchmark]
+        public IHtmlCollection<IElement> DivChildP() => _document.QuerySelectorAll("div > p");
+
+        [Benchmark]
+        public IHtmlCollection<IElement> DivClass() => _document.QuerySelectorAll("div[class]");
+
+        [Benchmark]
+        public IHtmlCollection<IElement> PFirstChild() => _document.QuerySelectorAll("p:first-child");
+
+        [Benchmark]
+        public IHtmlCollection<IElement> UlLiA() => _document.QuerySelectorAll("ul > li > a");
     }
 }
